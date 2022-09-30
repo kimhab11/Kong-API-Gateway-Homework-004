18114be31e723475f37055af1d118e458eda687e
kimhab <69948342+kimhab11@users.noreply.github.com>
2022-10-01 00:38:41 +0700

    c

--- kong-setup.sh
+++ kong-setup.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
 
+curl -i -X POST \
+--url http://localhost:8001/services/ \
+--data 'name=task-service' \
+--data 'url=http://110.74.194.123:6072' 
+
+curl -i -X POST \
+	--url http://localhost:8001/services/task-service/routes \
+	--data 'name=task-api' \
+	--data 'paths[]=/task' \
+	--data 'strip_path=true'
+
+curl -i -X POST \
+--url http://localhost:8001/services/ \
+--data 'name=user-service' \
+--data 'url=http://110.74.194.123:6070' 
+
+curl -i -X POST \
+	--url http://localhost:8001/services/user-service/routes \
+	--data 'name=user-api' \
+	--data 'paths[]=/user' \
+	--data 'strip_path=true'
